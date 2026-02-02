137c8ac1868c5154a001f6b50fdf9723f3cddf67
omshimpi43-ui <omshimpi43@gmail.com>
2026-02-02 18:14:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
+const screen1 = document.getElementById("screen1");
+const screen2 = document.getElementById("screen2");
+const startText = document.getElementById("startText");
+const stickman = document.getElementById("stickman");
+const message = document.getElementById("message");
+const crack = document.getElementById("crack");
+const ouch = document.getElementById("ouch");
+
+startText.onclick = () => {
+  screen1.classList.add("hidden");
+  screen2.classList.remove("hidden");
+};
+
+// Drag logic
+let draggedWeapon = null;
+
+document.querySelectorAll(".weapon").forEach(w => {
+  w.draggable = true;
+
+  w.ondragstart = e => {
+    draggedWeapon = w;
+  };
+});
+
+screen2.ondragover = e => e.preventDefault();
+
+screen2.ondrop = e => {
+  if (!draggedWeapon) return;
+
+  const stickRect = stickman.getBoundingClientRect();
+
+  if (
+    e.clientX > stickRect.left &&
+    e.clientX < stickRect.right &&
+    e.clientY > stickRect.top &&
+    e.clientY < stickRect.bottom
+  ) {
+    hitStickman(draggedWeapon.dataset.msg);
+  }
+};
+
+function hitStickman(text) {
+  stickman.style.transform = "rotate(90deg)";
+  crack.currentTime = 0;
+  ouch.currentTime = 0;
+  crack.play();
+  ouch.play();
+
+  message.textContent = text;
+  message.classList.remove("hidden");
+
+  setTimeout(() => {
+    stickman.style.transform = "rotate(0deg)";
+    message.classList.add("hidden");
+  }, 2000);
+}
 
